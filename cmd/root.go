418f33236dc6c952968008d7eda4d27413f71912
Milos Zivlak <zivlakmilos@gmail.com>
2025-02-27 17:03:36 +0100

    Use viper for config

--- cmd/root.go
+++ cmd/root.go
@@ -22,9 +22,11 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -38,6 +40,8 @@ Allow users to easly generate tax return for divident income.`,
 	},
 }
 
+var cfgFile string
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -48,6 +52,8 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -57,4 +63,42 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/rstax.json)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		cfgDir, err := os.UserConfigDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".rstax" (without extension).
+		viper.AddConfigPath(cfgDir)
+		viper.SetConfigType("json")
+		viper.SetConfigName("rstax")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
